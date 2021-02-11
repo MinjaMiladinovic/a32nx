fc383c72a8d10077b3215194453c4626c1c86832
Minja Miladinović <69137994+MinjaMiladinovic@users.noreply.github.com>
2021-02-11 16:26:10 +0100

    Update 11-2-NewFile1.js

--- 11-2-NewFile1.js
+++ 11-2-NewFile1.js
@@ -1,5 +1,8 @@
 <input type="text" id="price1">
 <input type="text" id="price2">
+<input type="text" id="price3">
+<input type="text" id="price4">
+<input type="text" id="price5">
 <input type="text" id="results">
 <input type="button" value="Calculate" onclick="calculateResults()">
 
@@ -7,7 +10,10 @@
 function calculateResults() {
     var price1Box = document.getElementById('price1');
     var price2Box = document.getElementById('price2');
+    var price3Box = document.getElementById('price3');
+    var price4Box = document.getElementById('price4');
+    var price5Box = document.getElementById('price5');
     var resultsBox = document.getElementById('results');
-    resultsBox.value = getTotalPrice(price1Box.value, price2Box.value);
+    resultsBox.value = getTotalPrice(price1Box.value, price2Box.value,price3Box.value, price4Box.value, price5Box.value);
 }
 </script>
