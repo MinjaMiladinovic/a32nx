97dbe46507f55345929a67c1306567a2b1c00165
Minja Miladinović <69137994+MinjaMiladinovic@users.noreply.github.com>
2021-02-11 16:10:55 +0100

    Create 11-2-NewFile1.js

---
+++ 11-2-NewFile1.js
@@ -0,0 +1,13 @@
+<input type="text" id="price1">
+<input type="text" id="price2">
+<input type="text" id="results">
+<input type="button" value="Calculate" onclick="calculateResults()">
+
+<script type="text/javascript">
+function calculateResults() {
+    var price1Box = document.getElementById('price1');
+    var price2Box = document.getElementById('price2');
+    var resultsBox = document.getElementById('results');
+    resultsBox.value = getTotalPrice(price1Box.value, price2Box.value);
+}
+</script>
