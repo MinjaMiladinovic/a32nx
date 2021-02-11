41e9e516b48ae7c665929b5235edf1697611dbd4
Minja Miladinović <69137994+MinjaMiladinovic@users.noreply.github.com>
2021-02-11 20:42:08 +0100

    Update 11.2.js

--- 11.2.js
+++ 11.2.js
@@ -1,6 +1,9 @@
-function person(name, lastname){
+function person(name, lastname, age, color, animal){
 	console.log("NAME: " + name);
 	console.log("LAST_NAME: " + lastname);
+    console.log("AGE: " + age);
+    console.log("color: " + color);
+    console.log("animal: " + animal);
 }
 
 person("adem", "hudi")
